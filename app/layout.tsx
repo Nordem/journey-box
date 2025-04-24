5b9f4d5d2cde7d561b44dd6cfaa441a0ee92878e
DianaAcostaM <dacosta@arkusnexus.com>
2025-04-24 09:54:09 -0600

    Beta banner added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,9 @@ import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/toaster'
 import { UserProfileProvider } from '@/lib/context/user-profile-context'
+import { BetaBanner } from '@/components/beta-banner'
 import Sidebar from '@/components/sidebar'
+import { usePathname } from 'next/navigation'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,6 +19,7 @@ export default function RootLayout({
 }>) {
   const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false)
   const [isMobile, setIsMobile] = useState(false) // Track mobile state
+  const pathname = usePathname()
 
   return (
     <html lang="es" suppressHydrationWarning>
@@ -30,24 +33,32 @@ export default function RootLayout({
         >
           <UserProfileProvider>
             <div className="flex h-screen">
+
               {/* Sidebar */}
-              <Sidebar
-                isAdmin={true}
-                isCollapsed={isSidebarCollapsed}
-                onToggleCollapse={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
-                onMobileChange={setIsMobile} // Pass callback to update mobile state
-              />
+              {pathname !== '/' && pathname != '/login' && ( // Only show Sidebar if not on the "/" page
+                <Sidebar
+                  isAdmin={true}
+                  isCollapsed={isSidebarCollapsed}
+                  onToggleCollapse={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
+                  onMobileChange={setIsMobile} // Pass callback to update mobile state
+                />
+              )}
 
               {/* Main Content */}
               <main
                 className={`flex-1 overflow-auto transition-all duration-300 ${
-                  isMobile
+                  pathname === '/' || pathname === '/login' // If on "/" page
+                    ? 'ml-0 mt-0' // No margin or padding
+                    : isMobile
                     ? 'ml-0 mt-6' // No margin when in mobile view
                     : isSidebarCollapsed
                     ? 'ml-[70px]'
                     : 'ml-[250px]'
                 }`}
               >
+                {/* Beta banner */}
+                <BetaBanner />
+
                 {children}
               </main>
             </div>
